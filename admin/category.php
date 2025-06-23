493008bcf5099f59ff3954ec0c637144b47e5252
Mohanatheesan <44610957+Mohanatheesan@users.noreply.github.com>
2025-06-23 09:48:23 +0530

    Add files via upload

---
+++ admin/category.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php require('inc/head.php'); ?>
+</head>
+
+<body>
+    <div class="wrapper">
+        <?php require('inc/sidebar.php'); ?>
+
+        <div class="main">
+            <?php require('inc/navbar.php'); ?>
+
+            <main class="content">
+                <div class="container-fluid p-0">
+
+                    <h1 class="h3 mb-3">Blank Page</h1>
+
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="card">
+                                <div class="card-header">
+                                    <h5 class="card-title mb-0">Empty card</h5>
+                                </div>
+                                <div class="card-body">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </main>
+
+            <?php require('inc/footer.php'); ?>
+        </div>
+    </div>
+
+    <script src="js/app.js"></script>
+
+</body>
+
+</html>
